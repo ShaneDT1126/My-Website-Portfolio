c7f18880665c1a8b012f9ad2137cfb9212f1f25a
Shane <shanedelatorre2412@gmail.com>
2024-07-31 20:40:09 +0800

    Add files aboutme and landingpage

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
 
-ReactDOM.createRoot(document.getElementById('main')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    Whats up bro
+    <App />
   </React.StrictMode>,
 )
