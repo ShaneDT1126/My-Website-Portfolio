59b337069ef526c72268b8228f2238e54194b146
Shane <shanedelatorre2412@gmail.com>
2024-07-31 19:48:19 +0800

    Change root ID to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('main')).render(
   <React.StrictMode>
     Whats up bro
   </React.StrictMode>,
