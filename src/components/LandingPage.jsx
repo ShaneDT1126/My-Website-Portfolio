4cd4d18b98ff94d27a29e6ecd21344e030358146
Shane <shanedelatorre2412@gmail.com>
2024-08-01 22:45:00 +0800

    Add header in LandingPage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,13 @@
-
+import '../components/LandingPage.css'
 
 export function LandingPage(){
     return (
         <div className="landing-page">
-            <h1>Welcome to the landing page</h1>
+            <header className="header">
+                <h1>Header</h1>
+                <img className="header-gif" align="right" width={'200px'} height={'200px'} alt="side_sticker" src="https://media.giphy.com/media/TEnXkcsHrP4YedChhA/giphy.gif" />
+            </header>
+            
         </div>
     )
 }
