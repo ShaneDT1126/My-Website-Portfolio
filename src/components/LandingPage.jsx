aacc44aa665e354bcb5a18c01192ae874c858355
Shane <shanedelatorre2412@gmail.com>
2024-08-01 23:04:29 +0800

    Update LandingPage content

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,8 +4,12 @@ export function LandingPage(){
     return (
         <div className="landing-page">
             <header className="header">
-                <h1>Header</h1>
-                <img className="header-gif" align="right" width={'200px'} height={'200px'} alt="side_sticker" src="https://media.giphy.com/media/TEnXkcsHrP4YedChhA/giphy.gif" />
+                <div className='left-content'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos corrupti itaque voluptatibus animi consequuntur nesciunt soluta vero laboriosam vitae asperiores eveniet, natus sit minus molestiae quas accusantium, est temporibus! Asperiores.
+                </div>
+                <div className='right-content'>
+                    <img className="header-gif" align="right" width={'400px'} height={'400px'} alt="side_sticker" src="https://media.giphy.com/media/TEnXkcsHrP4YedChhA/giphy.gif" />
+                </div>
             </header>
             
         </div>
