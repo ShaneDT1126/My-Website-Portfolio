9c38b326945895acdb69a4ec5856914f3f4acf77
Shane <shanedelatorre2412@gmail.com>
2024-08-01 23:26:52 +0800

    Update landingpage partially done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,13 @@ export function LandingPage(){
         <div className="landing-page">
             <header className="header">
                 <div className='left-content'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos corrupti itaque voluptatibus animi consequuntur nesciunt soluta vero laboriosam vitae asperiores eveniet, natus sit minus molestiae quas accusantium, est temporibus! Asperiores.
+                    <p className="header-text">
+                        Hello, <br/>
+                        I'm <span className='header-name'>Shane</span> - <br/>
+                        an aspiring <span className='header-content'>Software Engineer</span> <br/>
+                        w/a focus on <br/>
+                        <span className='header-content'>Full Stack Development</span>
+                    </p>
                 </div>
                 <div className='right-content'>
                     <img className="header-gif" align="right" width={'400px'} height={'400px'} alt="side_sticker" src="https://media.giphy.com/media/TEnXkcsHrP4YedChhA/giphy.gif" />
